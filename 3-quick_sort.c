ead33e2d7d802afa4dc922b0149e95cedebd48dc
Mathieu7483 <11436@holbertonstudents.com>
2025-08-06 12:09:53 +0200

    Fix typo in quick_sort function documentation and add function definition

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,8 +1,11 @@
 #include "sort.h"
 /**
-*quick_sor -  function that sorts an array of integers
+*quick_sort -  function that sorts an array of integers
 *in ascending order using the Quick sort algorithm
 *@array: pointer to the array
 *@size: size of the array
 */
 void quick_sort(int *array, size_t size)
+{
+
+}

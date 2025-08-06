c66e2260a802bf8c84ecdb7c6cffded8deed9018
Mathieu7483 <11436@holbertonstudents.com>
2025-08-06 12:09:20 +0200

    Implement quick sort algorithm for sorting an array of integers

---
+++ 3-quick_sort.c
@@ -0,0 +1,8 @@
+#include "sort.h"
+/**
+*quick_sor -  function that sorts an array of integers
+*in ascending order using the Quick sort algorithm
+*@array: pointer to the array
+*@size: size of the array
+*/
+void quick_sort(int *array, size_t size)
